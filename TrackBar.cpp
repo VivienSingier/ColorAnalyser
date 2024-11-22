5a6543bebe26e827c0121666dc378858569bddee
Vivien Singier <vsingier@gaming.tech>
2024-11-22 14:18:24 +0100

    Vfinale Bis

--- TrackBar.cpp
+++ TrackBar.cpp
@@ -8,12 +8,12 @@ TrackBar::TrackBar(int x, int y, HWND hWnd)
         x, y, 300, 30,
         hWnd, NULL, GetModuleHandle(NULL), NULL);
 
-    SendMessage(mWindow, TBM_SETRANGE, TRUE, MAKELPARAM(0, 255));
-    SendMessage(mWindow, TBM_SETPOS, TRUE, 122);
+    SendMessage(mWindow, TBM_SETRANGE, TRUE, MAKELPARAM(0, 200));
+    SendMessage(mWindow, TBM_SETPOS, TRUE, 100);
     mPosition = 0;
 
     mLabel = CreateWindowEx(
-        0, TEXT("STATIC"), TEXT("122"),
+        0, TEXT("STATIC"), TEXT("100"),
         WS_CHILD | WS_VISIBLE,
         x - 30, y + 5, 30, 20,
         hWnd, NULL, GetModuleHandle(NULL), NULL);
